8da5d5966026922cbf3620a3d3363f487b259284
billstrand1@gmail.com <billstrand1@gmail.com>
2025-05-13 16:34:06 +0000

    Edited HelpLoggedIn

--- client_code/HelpLoggedIn/__init__.py
+++ client_code/HelpLoggedIn/__init__.py
@@ -14,10 +14,10 @@ class HelpLoggedIn(HelpLoggedInTemplate):
     self.init_components(**properties)
     
     #------------------VERIFY FALSE AFTER TESTING
-    DEBUG = True
-    if DEBUG:
-      print("Calling for log-in, DON'T FORGET TO set DEBUG=False")
-      anvil.server.call('force_debug_login_shr_utils')
+    # DEBUG = True
+    # if DEBUG:
+    #   print("Calling for log-in, DON'T FORGET TO set DEBUG=False")
+    #   anvil.server.call('force_debug_login_shr_utils')
     
     user = anvil.users.get_user()
     full_name = f"{user['first_name']} {user['last_name']}"
