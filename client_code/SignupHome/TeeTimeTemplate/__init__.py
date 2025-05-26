12cf9bd4fce3c78f114af160d51acb0b495871c0
billstrand1@gmail.com <billstrand1@gmail.com>
2025-05-26 15:13:02 +0000

    Created SignupHome.TeeTimeTemplate.Form1, SignupHome.TeeTimeTemplate, SignupHome.TeeTimeTemplate.RowTemplate1 and tee_times; Edited SignupHome and settings

--- client_code/SignupHome/TeeTimeTemplate/__init__.py
+++ client_code/SignupHome/TeeTimeTemplate/__init__.py
@@ -1,16 +1,15 @@
-from ._anvil_designer import SignupHomeTemplate
+from ._anvil_designer import TeeTimeTemplateTemplate
 from anvil import *
 import anvil.server
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from .TeeTimeTemplate import TeeTimeTemplate
 
-
-class SignupHome(SignupHomeTemplate):
+class TeeTimeTemplate(TeeTimeTemplateTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.signup_panel.add_component('TeeTimeTemplate')
+
+    self.repeating_panel_tee_times.items = anvil.server.call('get_friday_players')
     # Any code you write here will run before the form opens.
---
+++ server_code/tee_times.py
@@ -0,0 +1,12 @@
+import anvil.email
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+@anvil.server.callable
+def get_friday_players():
+  
+  
+  
